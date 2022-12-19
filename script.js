d9455d807a6fccb7cfa799aff8812ac011ff0645
elian <elian.bourdu@gmail.com>
2022-12-19 13:21:31 +0000

    add player sign feature on click

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 // All ways of winning
 const checkWin = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
+    [0, 4, 8],
+    [2, 4, 6]
 ];
-// push ou je fou le bordel deadline 12:30
+
 let count = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
-    const arrayOfButton = document.getElementsByTagName("button");
-    Array.from(arrayOfButton).forEach(button => {
+    const arrayOfButton = Array.from(document.getElementsByTagName("button"));
+    console.log(arrayOfButton)
+    arrayOfButton.forEach(button => {
         const onclick = () => {
-            console.log(count++);
-            button.innerText = "X"
-            button.removeEventListener('click', onclick)
-        }
+            button.removeEventListener('click', onclick);
+            count++;
+            console.log(count);
+            if (count % 2 === 0) {
+                // even
+                button.innerText = "o";
+            }
+            else if (count % 2 === 1) {
+                // odd
+                button.innerText = "x";
+            }
+
+            // check if a player win after each click
+            // const checkIfPlayerWin = (sign) => {
+            //     for (let i = 0; i < checkWin.length; i++) {
+            //         if (checkWin[i]) {
+
+            //         }
+            //     }
+            // };
+        };
         button.addEventListener('click', onclick);
     });
 });

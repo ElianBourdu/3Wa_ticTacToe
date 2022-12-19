ef28319e91c3d0ca3a589dea53f7418c31665d17
elian <elian.bourdu@gmail.com>
2022-12-19 11:22:30 +0000

    onclick each case done

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // All ways of winning
 const checkWin = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
 ];
-    
+// push ou je fou le bordel deadline 12:30
+let count = 0;
+
 document.addEventListener('DOMContentLoaded', () => {
-    
-})
+    const arrayOfButton = document.getElementsByTagName("button");
+    Array.from(arrayOfButton).forEach(button => {
+        const onclick = () => {
+            console.log(count++);
+            button.innerText = "X"
+            button.removeEventListener('click', onclick)
+        }
+        button.addEventListener('click', onclick);
+    });
+});
